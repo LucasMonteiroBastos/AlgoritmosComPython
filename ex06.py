4aff50bd504182f5b26174e1d6636788cf72f2fd
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-25 09:54:16 -0300

    condicional aluno aprovado / recuperação / reprovado

--- ex06.py
+++ ex06.py
@@ -9,6 +9,6 @@ media = (a + b) /2
 if media >= 7:
     print('{}, a sua media é {}, PARABENS APROVADO'.format(nome,media))
 elif media >= 5 and media < 7:
-    print('{} sua media é {}, Voce esta de RECUPERAÇÃO, estude mais.'.format(nome,media))
+    print('{}, sua media é {}, Voce esta de RECUPERAÇÃO, estude mais.'.format(nome,media))
 elif media < 5:
     print('{}, sua media é {}, infelizmente voce esta REPROVADO.'.format(nome,media))
