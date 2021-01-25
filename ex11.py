fc4d4ba1130a15d807bd49c67d39b3203ee3767f
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-25 11:58:53 -0300

    repetição while, fazendo soma entre numeros e indicando o maior valor informado.

--- ex11.py
+++ ex11.py
@@ -2,10 +2,14 @@ print('somar quantas vezes? ')
 vezes = int(input())
 contador = 0
 resultado = 0
-
+maior = 0
 while(contador < vezes):
     contador += 1
     print('informe o valor da soma: ')
     a = int(input())
+    if a > maior:
+        maior = a
     resultado += a
+
 print('A soma dos numeros foi = {}'.format(resultado))
+print('O maior numero é {}'.format(maior))
