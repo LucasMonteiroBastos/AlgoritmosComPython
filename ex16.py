3aa5bb0354cbb1591117992b8eacfd94572ccdbc
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-29 12:46:50 -0300

    vetor com numeros pre definidos e mostrando o indeces.

---
+++ ex16.py
@@ -0,0 +1,5 @@
+# Preenchendo o vetor atraves de imputs.
+vetor = []
+for count in range(0,6):
+    vetor.append(int(input('Informe os valores do vetor: ')))
+print('Esses são os valores contidos no vetor: {}'.format(vetor))
