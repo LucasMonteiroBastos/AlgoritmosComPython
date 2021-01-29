36d01e8505c0fcb45d7cff6f98139994bd332e05
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-29 12:46:01 -0300

    vetor com numeros pre definidos e mostrando o indeces.

---
+++ ex15.py
@@ -0,0 +1,9 @@
+# vetor com numeros pre definidos e mostrando o indeces.
+valores = []
+valores.append(10)
+valores.append(20)
+valores.append(30)
+
+for c, v in enumerate(valores):
+    print(f'Indice {c} valor {v}...')
+print('Fim!')
